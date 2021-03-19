c407cb97bdc4236f7f322896b102b2a521d06462
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-03-18 22:02:16 -0500

    Peng: Added more variables.

--- encoding-decoding.py
+++ encoding-decoding.py
@@ -5,3 +5,11 @@ root = Tk()
 root.geometry('500x300')
 root.resizable(0,0)
 root.title("DataFair = Message Encode and Decode")
+
+Label(root, text = 'ENCODE DECODE', font = 'arial 20 bold').pack()
+Label(root, text = 'DataFlair', font = 'arial 20 bold').pack(side = BOTTOM)
+
+Text = StringVar()
+private_key = StringVar()
+mode = StringVar()
+Result = StringVar()
