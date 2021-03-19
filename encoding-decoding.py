31f96a42c9dfe3823913e1d75dee03cb9d40c994
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-03-18 22:52:04 -0500

    Peng: Added two new functions and some buttons.

--- encoding-decoding.py
+++ encoding-decoding.py
@@ -41,3 +41,33 @@ def Decode(key,message):
         key_c = key[i % len(message)]
         dec.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
     return "".join(dec)
+
+# Mode
+''' The Mode function is used to get the result of the code either being encoded or decoded '''
+def Mode():
+    if(mode.get() == 'e'):
+        Result.set(Encode(private_key.get(), Text.get()))
+    elif(mode.get() == 'd'):
+        Result.set(Decode(private_key.get(), Text.get()))
+    else:
+        Result.set('Invalid Result')
+
+# Clear
+''' The Clear function is used to clear the entire input and output '''
+def Clear():
+    Text.set("")
+    private_key.set("")
+    mode.set("")
+    Result.set("")
+
+Label(root, font= 'arial 12 bold', text='MESSAGE').place(x= 60,y=60)
+Entry(root, font = 'arial 10', textvariable = Text, bg = 'ghost white').place(x=290, y = 60)
+Label(root, font = 'arial 12 bold', text ='KEY').place(x=60, y = 90)
+Entry(root, font = 'arial 10', textvariable = private_key , bg ='ghost white').place(x=290, y = 90)
+Label(root, font = 'arial 12 bold', text ='MODE(e-encode, d-decode)').place(x=60, y = 120)
+Entry(root, font = 'arial 10', textvariable = mode , bg= 'ghost white').place(x=290, y = 120)
+Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='ghost white').place(x=290, y = 150)
+Button(root, font = 'arial 10 bold', text = 'RESULT'  ,padx =2,bg ='LightGray' ,command = Mode).place(x=60, y = 150)
+Button(root, font = 'arial 10 bold' ,text ='RESET' ,width =6, command = ConnectionResetError, bg = 'LimeGreen', padx=2).place(x=80, y = 190)
+Button(root, font = 'arial 10 bold',text= 'EXIT' , width = 6, command = Exit,bg = 'OrangeRed', padx=2, pady=2).place(x=180, y = 190)
+root.mainloop()
